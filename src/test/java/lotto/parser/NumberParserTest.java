c8a2f0f22a0127e3c37007204e8cc38f60565a56
etd93 <etd93@naver.com>
2025-11-01 23:56:36 +0900

    test(NumberParserTest): 빈 문자열이거나 공백 입력이면 예외 발생 테스트 추가

---
+++ src/test/java/lotto/parser/NumberParserTest.java
@@ -0,0 +1,23 @@
+package lotto.parser;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NumberParserTest {
+
+    @DisplayName("빈 문자열을 파싱하려 하면 예외가 발생한다.")
+    @Test
+    void parse_withEmptyString_shouldThrowException() {
+        assertThatThrownBy(() -> NumberParser.parse(""))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("공백만 있는 문자열을 파싱하려 하면 예외가 발생한다.")
+    @Test
+    void parse_withWhitespaceString_shouldThrowException() {
+        assertThatThrownBy(() -> NumberParser.parse("   "))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
