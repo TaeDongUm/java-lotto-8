7a06dc01e4c34d7e1b500ab58cacc8ffc7c4bf7e
etd93 <etd93@naver.com>
2025-11-02 18:41:42 +0900

    test(LottoMachineTest): 요청 장수만큼 로또 리스트가 반환되는지 테스트 추가

--- src/test/java/lotto/service/LottoMachineTest.java
+++ src/test/java/lotto/service/LottoMachineTest.java
@@ -1,8 +1,12 @@
 package lotto.service;
 
+import lotto.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoMachineTest {
@@ -16,5 +20,19 @@ class LottoMachineTest {
         // when & then
         assertThatThrownBy(() -> lottoMachine.generate(0))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("요청한 장수만큼 로또 리스트를 반환한다.")
+    @Test
+    void generateLottos_withValidCount_shouldReturnLottoList() {
+        // given
+        LottoMachine lottoMachine = new LottoMachine();
+        int count = 5;
+
+        // when
+        List<Lotto> lottos = lottoMachine.generate(count);
+
+        // then
+        assertThat(lottos).hasSize(count);
     }
 }
